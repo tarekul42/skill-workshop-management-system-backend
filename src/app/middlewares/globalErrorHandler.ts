942347ab3d5663336edee85cbbfc29ae5d30940b
tarekul42 <tarekulrifat205@gmail.com>
2026-01-12 13:52:38 +0600

    feat: custom error class, AppError

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,6 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import { NextFunction, Request, Response } from "express";
 import envVariables from "../config/env";
+import AppError from "../errorHelpers/AppError";
 
 const globalErrorHandler = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -10,8 +11,16 @@ const globalErrorHandler = (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) => {
-  const statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
-  const message = `Something went wrong!!! ${err.message}`;
+  let statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
+  let message = `Something went wrong!!! ${err.message}`;
+
+  if (err instanceof AppError) {
+    statusCode = err.statusCode;
+    message = err.message;
+  } else if (err instanceof Error) {
+    statusCode = 500;
+    message = err.message;
+  }
 
   res.status(statusCode).json({
     success: false,
