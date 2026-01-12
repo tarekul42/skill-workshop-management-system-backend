1b1d342a38a2f9d884bed24c88d0fdc87ca9bede
tarekul42 <tarekulrifat205@gmail.com>
2026-01-12 09:16:05 +0600

    feat: createuser controller and route added and implemented on main app.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response } from "express";
+import { UserRoutes } from "./app/modules/user/user.routes";
+import cors from "cors";
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1/user", UserRoutes);
 
 app.get("/", (_req: Request, res: Response) => {
   res.status(200).json({
---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from "express";
+import httpStatusCodes from "http-status-codes";
+import User from "./user.model";
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const { name, email } = req.body;
+    const result = User.create({
+      name,
+      email,
+    });
+    res
+      .status(httpStatusCodes.CREATED)
+      .json({ message: "User created successfuly", result });
+  } catch (error: any) {
+    res
+      .status(httpStatusCodes.BAD_REQUEST)
+      .json({ message: `Something went wrong!! ${error.message}` });
+  }
+};
+
+export const UserController = {
+  createUser,
+};
