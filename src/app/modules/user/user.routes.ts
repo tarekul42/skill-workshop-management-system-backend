43bb82b53f381bc9c38ff6c753881e5d455ebc1b
tarekul42 <tarekulrifat205@gmail.com>
2026-01-14 03:10:05 +0600

    feat: utility function added for validating users data

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import UserControllers from "./user.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { createUserZodSchema } from "./user.validation";
 
 const router = Router();
 
-router.post("/register", UserControllers.createUser);
+router.post(
+  "/register",
+  validateRequest(createUserZodSchema),
+  UserControllers.createUser
+);
 router.get("/all-users", UserControllers.getAllUsers);
 
 const UserRoutes = router;
