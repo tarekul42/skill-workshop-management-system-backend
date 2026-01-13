96e25e214cba73201d5bcd03f3522105e2978a25
tarekul42 <tarekulrifat205@gmail.com>
2026-01-14 02:32:10 +0600

    feat: zod validation for update user schema

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,4 +1,5 @@
 import z from "zod";
+import { IsActive, UserRole } from "./user.interface";
 
 const createUserZodSchema = z.object({
   name: z
@@ -29,10 +30,54 @@ const createUserZodSchema = z.object({
         "Phone number must be valid for Bangladesh. Format: +8801XXXXXXXXX or 01XXXXXXXXX",
     })
     .optional(),
+  age: z
+    .number("Age must be a number")
+    .min(0, { message: "Age cannot be negative." })
+    .optional(),
   address: z
     .string("Address must be string")
     .max(200, { message: "Address cannot exceed 200 characters." })
     .optional(),
 });
 
-export { createUserZodSchema };
+const updateUserZodSchema = z.object({
+  name: z
+    .string("Name must be string")
+    .min(2, { message: "Name must be at least 2 characters long." })
+    .max(50, { message: "Name cannot exceed 50 characters." })
+    .optional(),
+  password: z
+    .string("Password must be string")
+    .min(8, { message: "Password must be at least 8 characters long." })
+    .regex(/^(?=.*[A-Z])/, {
+      message: "Password must contain at least 1 uppercase letter.",
+    })
+    .regex(/^(?=.*[!@#$%^&*])/, {
+      message: "Password must contain at least 1 special character.",
+    })
+    .regex(/^(?=.*\d)/, {
+      message: "Password must contain at least 1 number.",
+    })
+    .optional(),
+  phone: z
+    .string("Phone Number must be string")
+    .regex(/^(?:\+8801\d{9}|01\d{9})$/, {
+      message:
+        "Phone number must be valid for Bangladesh. Format: +8801XXXXXXXXX or 01XXXXXXXXX",
+    })
+    .optional(),
+  age: z
+    .number("Age must be a number")
+    .min(0, { message: "Age cannot be negative." })
+    .optional(),
+  address: z
+    .string("Address must be string")
+    .max(200, { message: "Address cannot exceed 200 characters." })
+    .optional(),
+  isDeleted: z.boolean("isDeleted must be true or false").optional(),
+  isActive: z.enum(Object.values(IsActive) as [string]).optional(),
+  isVerified: z.boolean("isVerified must be true or false").optional(),
+  role: z.enum(Object.values(UserRole) as [string]).optional(),
+});
+
+export { createUserZodSchema, updateUserZodSchema };
