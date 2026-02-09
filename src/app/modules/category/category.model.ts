5bd15f71b93bb98ad43793f3e4e0806111a4fb31
tarekul42 <tarekulrifat205@gmail.com>
2026-02-09 09:03:26 +0600

    feat: Add category and workshop interfaces and models

---
+++ src/app/modules/category/category.model.ts
@@ -0,0 +1,18 @@
+import { model, Schema } from "mongoose";
+import { ICategory } from "./category.interface";
+
+const categorySchema = new Schema<ICategory>(
+  {
+    name: { type: String, required: true, unique: true },
+    slug: { type: String, unique: true },
+    thumbnail: { type: String },
+    description: { type: String },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Category = model<ICategory>("Category", categorySchema);
+
+export { Category };
