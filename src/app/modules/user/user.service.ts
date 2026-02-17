a4ec2c61e38f6c939c025c6d2cd1d9d9ed628afb
tarekul42 <tarekulrifat205@gmail.com>
2026-02-18 03:14:47 +0600

    feat: implement user retrieval features with search and pagination support

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -6,6 +6,8 @@ import { IAuthProvider, IUser, UserRole } from "./user.interface";
 import User from "./user.model";
 import bcrypt from "bcryptjs";
 import { JwtPayload } from "jsonwebtoken";
+import QueryBuilder from "../../utils/queryBuilder";
+import { userSearchableFields } from "./user.constant";
 
 const createUser = async (payload: Partial<IUser>) => {
   const { name, email, password, ...rest } = payload;
@@ -43,6 +45,37 @@ const createUser = async (payload: Partial<IUser>) => {
   });
 
   return user;
+};
+
+const getSingleUser = async (id: string) => {
+  const user = await User.findById(id);
+
+  if (!user) {
+    throw new AppError(StatusCodes.NOT_FOUND, "User not found");
+  }
+
+  return { data: user };
+};
+
+const getAllUsers = async (query: Record<string, string>) => {
+  const queryBuilder = new QueryBuilder(User.find(), query);
+
+  const usersData = queryBuilder
+    .search(userSearchableFields)
+    .filter()
+    .sort()
+    .fields()
+    .paginate();
+
+  const [data, meta] = await Promise.all([
+    usersData.build(),
+    queryBuilder.getMeta,
+  ]);
+
+  return {
+    data,
+    meta,
+  };
 };
 
 const updateUser = async (
@@ -122,22 +155,11 @@ const updateUser = async (
   return updatedUser;
 };
 
-const getAllUsers = async () => {
-  const users = await User.find({});
-  const totalUsers = await User.countDocuments();
-
-  return {
-    data: users,
-    meta: {
-      total: totalUsers,
-    },
-  };
-};
-
 const UserServices = {
   createUser,
-  updateUser,
+  getSingleUser,
   getAllUsers,
+  updateUser,
 };
 
 export default UserServices;
